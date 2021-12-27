30ca8e87868fa0ac33fce0ecdfaf992406ac07ea
nurdPi <96603736+nurdPi@users.noreply.github.com>
2021-12-27 12:56:46 -0500

    Added comments and updated variable names

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-document.body.innerHTML = `
-<img src="logo.png" width="${innerWidth - 5}" height="${innerWidth * 0.0851165002}"></img>
+  /* set up display */
+document.body.innerHTML = 
+`
+<img src="logo.png" width="${innerWidth - 5}" height="${innerWidth * 0.0851165002}"></img><img src="info.png"></img>
 <div id="list" levels>${display()}</div><br><br>
 <div pusab big>Website by nurdPi, List by Paintball</div>
 `
+  /* display function, turns level data into html */
 function display() {
   var
-    lol = ''
-    ;
+    output = ''
+  ;
   for (i in list) {
-    item = list[i];
-    piss = ""
+      /* variables */
+    var 
+      item = list[i]
+      , topbottom = ""
+      , bgcol = ""
+    ;
+    
+      /* styling */
+    // if i is an even number then the background will be darker
     if (Math.floor(i / 2) == i / 2) {
-      ass = "b"
+      bgcol = "b";
     }
     else {
-      ass = "a"
-    }
-    if (i == 0) {
-      piss += "top"
-    }
-    if (i == list.length - 1) {
-      piss += " bottom"
+      bgcol = "a";
     }
     
-    lol += `
-      <div ${piss} ${ass} level><div arial normal>#${parseInt(i)+1}: <a href="http://${item[2]}"><color blue>${item[0]}</color></a> ${item[1]} <br> <div small>ID: ${item[3]}; FPS: ${item[4]}; WR: ${item[5]}</div></div></div>
+    // if we're on the first level then smooth out the top corners
+    if (i == 0) {
+      topbottom = "top";
+    }
+    // if we're on the last level then smooth out the bottom corners
+    if (i == list.length - 1) {
+      topbottom = "bottom";
+    }
+    
+    /* and then we lay out the information into a set format */
+    output += `
+      <div ${topbottom} ${bgcol} level>                                                   // level container
+        <div arial normal>                                                                // level info
+          #${parseInt(i)+1}:                                                              // level placement
+          <a href="http://${item[2]}">                                                    // level video
+            <color blue>${item[0]}</color>                                                // level name (blue color because it's a link)
+          </a>                                                                       
+          ${item[1]} <br> <div small>ID: ${item[3]}; FPS: ${item[4]}; WR: ${item[5]}      // misc level data
+        </div>
+      </div>
+    </div>
     `;
   }
-  return lol;
+  return output;
 }
