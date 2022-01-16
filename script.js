b70d1d79ee5f40c55d53d7134acbd926bcc671c5
nurdPi <nurd3.14159@gmail.com>
2022-01-16 14:00:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -83,11 +83,11 @@ function display() {
     output += `
       <div ${topbottom} ${bgcol} level>                                                   <!--- level container                                               -->
         <div arial normal>                                                                <!--- level info                                                    -->
-          #${parseInt(i)-1}:                                                              <!--- level placement                                               -->
+          #${parseInt(i)-countstart}:                                                     <!--- level placement                                               -->
           <a href="youtu.be/${item[2] || "dQw4w9WgXcQ"}">                                 <!--- level video                                                   -->
             <color blue>${item[0]}</color>                                                <!--- level name (blue color because it's a link)                   -->
           </a>                                                                            <!--- I just wanted to put a comment here for aesthetic reasons lol -->
-          by ${item[1]} <br> <div small>ID: ${item[3]}; FPS: ${item[4]}; WR: ${item[5]}      <!--- misc level data                                               -->
+          by ${item[1]} <br> <div small>ID: ${item[3]}; FPS: ${item[4]}; WR: ${item[5]}   <!--- misc level data                                               -->
         </div>
       </div>
     </div>
